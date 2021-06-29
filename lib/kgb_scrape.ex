8d9466dd58c827de934c6c083671a89f31bebfad
Justinbenfit23 <64285817+Justinbenfit23@users.noreply.github.com>
2021-06-28 21:57:47 -0600

    Add files via upload
    initial commit

---
+++ lib/kgb_scrape.ex
@@ -0,0 +1,25 @@
+defmodule KgbScrape do
+
+  def get_reviews_url() do
+    case HTTPoison.get("https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/#link") do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        urls =
+            body
+            |> Floki.find("div.col-xs-12.review-section.pad-md.pad-top-none.pad-bottom-none.mobile-hide")
+            |> Enum.map(&Floki.text/1)
+            
+            #figure out how to get all the text to print.
+            
+            
+            
+
+        {:ok, urls}
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "Not found :("
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+
+        end
+  end
+end
+
---
+++ test/kgb_scrape_test.exs
@@ -0,0 +1,8 @@
+defmodule KgbScrapeTest do
+  use ExUnit.Case
+  doctest KgbScrape
+
+  test "greets the world" do
+    assert KgbScrape.hello() == :world
+  end
+end
