14429da2af630add95b181ff19053e1c029ec453
Justinbenfit23 <64285817+Justinbenfit23@users.noreply.github.com>
2021-07-12 08:39:25 -0600

    Update kgb_scrape.ex
    added to module to get top 3 most positive reviews

--- lib/kgb_scrape.ex
+++ lib/kgb_scrape.ex
@@ -1,20 +1,58 @@
 defmodule KgbScrape do
-
-  def get_reviews_url() do
-     case HTTPoison.get("https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/#link") do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        IO.puts body
-        |> Floki.find("div.col-xs-12.review-section.pad-md.pad-top-none.pad-bottom-none.mobile-hide")
-        |> Enum.map(&Floki.text/1)
-
-
-
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts "Not found :("
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
+    require Logger
+    use HTTPoison.Base
+    @endpoint "https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/page"
+  
+    def get_body() do
+      page_num = ["1","2","3","4","5"]
+      tail_url = ["/?filter=#link"]
+      urls_list = for page <- page_num, tail <- tail_url do
+        @endpoint <> page <> tail
+      end
+      urls_list
+      |> Enum.map(fn url -> handle_response(url) end)
+      |> Floki.parse
+      |> Floki.find(".review-entry")
+      |> Enum.map(fn entry ->
+        [{"div", _, [date]}] = Floki.find(entry, "div.italic")
+        [{"p", _, [content]}]  = Floki.find(entry, "p.review-content")
+        {date, content} 
+      end)
     end
-  end
+
+    def calc_score() do
+      
+      reviews = get_body()
+      special_words = ["extremely", "definitely","amazing","very","best","great","excellent","awesome","incredibly","beyond","loved","really","highly"]
+
+      compute_score = fn review -> {special, total} = review |> String.downcase |> String.split |> Enum.reduce({0, 0}, fn w, {sp, to} -> if w in special_words, do: {sp + 1, to + 1}, else: {sp, to + 1} end)                                                              
+      special/total
+      end
+
+      score = for {d, r} <- reviews, do: %{id: :erlang.phash2(r), date: d, score: compute_score.(r), text: r}
+      Enum.sort_by(score, fn(s) -> s.score end) |> Enum.reverse() |> Enum.take(3)
+
+      
+
+    end
+    
+     
+    def handle_response(url) do
+      case HTTPoison.get(url) do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+          body
+        {:ok, %HTTPoison.Response{status_code: status_code}} ->
+          Logger.info(fn ->
+            "[#{__MODULE__}] Hit url: #{url}, status_code: #{inspect(status_code)}"
+          end)
+          {:ok, "Failed to scrape website. Status code: #{inspect(status_code)}"}
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          Logger.info(fn ->
+            "[#{__MODULE__}] Failed to scrape website: #{inspect(reason)}"
+          end)
+          {:ok, "Failed to scrape website. Reason: #{inspect(reason)}}"}
+      end
+    end
 end
 
 
